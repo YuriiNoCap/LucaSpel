010dbd655b8594604c921a070186196a79e072ba
Yurii <yurii.pustovit@skola.taby.se>
2023-04-03 15:11:52 +0200

    fghjk

--- spel.js
+++ spel.js
@@ -1,40 +1,56 @@
 //  ------------ Setup ------------
 window.focus;
 
-let gameCanvas = document.getElementById("gameCanvas");
-let c = gameCanvas.getContext("2d"); // Drawing object
+const gameCanvas = document.getElementById("gameCanvas");
+const c = gameCanvas.getContext("2d"); // Drawing object
 gameCanvas.height = window.innerHeight;
 gameCanvas.width = window.innerWidth;
+
+// Spel plan
+function spelPlan() {
+  c.beginPath();
+  c.moveTo(0, 520);
+  c.lineTo(gameCanvas.width, 520);
+  c.stroke();
+}
+
 // -------------------------------------
 // Player variables
-let playerX = 100;
-let playerY = 100;
-let playerWidth = 10;
-let playerHeight = 10;
-let dx = 2;
-let dy = 2;
 
-let directions = {
-  left: false,
-  right: false,
-  up: false,
-  down: false,
+let player = {
+  x: 10,
+  y: 10,
+  playerWidth: 10,
+  playerHeight: 10,
+  //   y: window.innerHeight / 2,
+  dx: 3,
+  dy: 3,
+
+  //   img: document.getElementById("hero"),
+  height: 60,
+  width: 60,
+  directions: {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+  },
 };
 // -------------------------------------
 // ------------ Player movement ------------
 document.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowLeft":
-      directions.left = true;
+      player.directions.left = true;
       break;
     case "ArrowRight":
-      directions.right = true;
+      player.directions.right = true;
       break;
     case "ArrowUp":
-      directions.up = true;
+      player.directions.up = true;
       break;
     case "ArrowDown":
-      directions.down = true;
+      player.directions.down = true;
       break;
     default:
       break;
@@ -44,16 +60,16 @@ document.addEventListener("keydown", (e) => {
 document.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "ArrowLeft":
-      directions.left = false;
+      player.directions.left = false;
       break;
     case "ArrowRight":
-      directions.right = false;
+      player.directions.right = false;
       break;
     case "ArrowUp":
-      directions.up = false;
+      player.directions.up = false;
       break;
     case "ArrowDown":
-      directions.down = false;
+      player.directions.down = false;
       break;
     default:
       break;
@@ -64,23 +80,23 @@ document.addEventListener("keyup", (e) => {
 function animate() {
   requestAnimationFrame(animate); // Run gameloop recursively
   c.clearRect(0, 0, gameCanvas.width, gameCanvas.height); // Clear screen
+  spelPlan();
+  c.fillRect(player.x, player.y, player.playerWidth, player.playerHeight); // Draw player
 
-  c.fillRect(playerX, playerY, playerWidth, playerHeight); // Draw player
-
-  if (directions.right) {
-    playerX += dx;
+  if (player.directions.right) {
+    player.x += player.dx;
   }
 
-  if (directions.left) {
-    playerX -= dx;
+  if (player.directions.left) {
+    player.x -= player.dx;
   }
 
-  if (directions.up) {
-    playerY -= dy;
+  if (player.directions.up) {
+    player.y -= player.dy;
   }
 
-  if (directions.down) {
-    playerY += dy;
+  if (player.directions.down) {
+    player.y += player.dy;
   }
 }
 // -------------------------------------
